0bf07b833b40de31588cb0662c9e04b95e0e7fe2
A I <atbisrael@gmail.com>
2018-01-05 00:28:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(array){
+ var newarr = [];
+ for (let i = 0; i < 25; i++){
+ greeting = "`I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.`";
+ newarr[i] = greeting;
+  }
+ return newarr;
+}
+
+function whileLoop(n){
+  while (n > 0) {
+    
+  console.log(n);
+  --n;
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+  do{ 
+    // for (let i = 1; i < array.length; ++i){
+      array.pop();
+  //  }
+  }
+  while (array.length != 0);
+    
+  
+}
